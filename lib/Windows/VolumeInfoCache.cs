487f80e6790fba66aa893cd3a8e3b027de3aada2
Erik Mavrinac <erikmav@microsoft.com>
2024-03-25 14:25:28 -0700

    Add an error handling case to ignore volumes that return File Not Found (#41)
    For https://github.com/microsoft/MSBuildSdks/issues/546

--- lib/Windows/VolumeInfoCache.cs
+++ lib/Windows/VolumeInfoCache.cs
@@ -103,6 +103,7 @@ internal sealed class VolumeInfoCache
                                     "If the drive was added recently you may need to recreate the filesystem cache.");
     }
 
+    private const int ERROR_FILE_NOT_FOUND = 2;
     private const int ERROR_ACCESS_DENIED = 5;
     private const int ERROR_NOT_READY = 21;
     private const int ERROR_INVALID_PARAMETER = 87;
@@ -129,11 +130,13 @@ internal sealed class VolumeInfoCache
             // - BitLocker can have a volume locked.
             // - Access denied can imply a volume needing escalated privilege to get its metadata,
             //   sometimes indicating a Windows container volume.
+            // - Not found can occur as a timing issue on some machines.
             if (lastErr == ERROR_UNRECOGNIZED_VOLUME ||
                 lastErr == ERROR_NOT_READY ||
                 lastErr == ERROR_INVALID_PARAMETER ||
                 lastErr == FVE_E_LOCKED_VOLUME ||
-                lastErr == ERROR_ACCESS_DENIED)
+                lastErr == ERROR_ACCESS_DENIED ||
+                lastErr == ERROR_FILE_NOT_FOUND)
             {
                 return null;
             }
